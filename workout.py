b2f8f1dc47178374d2857ac571fad522c2951f38
alnibo <alexander@bode-web.eu>
2020-01-04 17:11:11 +0000

    changed debug

--- workout.py
+++ workout.py
@@ -1,13 +1,30 @@
 import os
-from flask import Flask
+from flask import Flask, render_template
+from flask_pymongo import PyMongo
+from bson.objectid import ObjectId
 
 app = Flask(__name__)
 
+app.config["MONGO_URI"] = os.getenv("MONGO_URI")
+app.config["MONGO_DBNAME"] = 'workout'
+
+"""
+MONGODB_URI = os.getenv("MONGO_URI")
+DBS_NAME = "workout"
+COLLECTION_NAME = "exercises"
+"""
+
+mongo = PyMongo(app)
+
 @app.route('/')
-def hello():
-    return "Hello World"
+@app.route('/index')
+def index():
+    """
+    Allows user to view the home page, where they can select among the workout groups.
+    """
+    return render_template('index.html')
 
 if __name__ == '__main__':
     app.run(host=os.environ.get('IP'),
             port=int(os.environ.get('PORT')),
-            debug=False)
+            debug=True)
