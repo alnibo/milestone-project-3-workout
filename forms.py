4223789b0e682eef57d2de75565df94f9a923b56
alnibo <alexander@bode-web.eu>
2020-03-06 00:49:54 +0000

    updated select field in forms

--- forms.py
+++ forms.py
@@ -2,6 +2,7 @@ from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField, SelectField, TextAreaField
 from wtforms.validators import ValidationError, DataRequired, EqualTo, Length
 
+
 # Login Form
 
 class LoginForm(FlaskForm):
@@ -9,26 +10,43 @@ class LoginForm(FlaskForm):
     password = PasswordField('Password', validators=[DataRequired()])
     submit = SubmitField('Login')
 
+
 # Registration Form
 
 class RegistrationForm(FlaskForm):
-    username = StringField('Username', validators=[DataRequired(), Length(min=3, max=25)])
-    password = PasswordField('Password', validators=[DataRequired(), Length(min=5, max=50)])
-    password2 = PasswordField('Confirm Password', validators=[DataRequired(), EqualTo('password')])
+    username = StringField('Username', validators=[DataRequired(),
+                                                   Length(min=3, max=25)])
+    password = PasswordField('Password', validators=[DataRequired(),
+                                                     Length(min=5, max=50)])
+    password2 = PasswordField('Confirm Password',
+                              validators=[DataRequired(), EqualTo('password')])
     submit = SubmitField('Register')
 
-# Add Exercise Form
 
+# Add Exercise Form
+"""
 class ExerciseForm(FlaskForm):
-    category_name = SelectField('Exercise Group', validators=[DataRequired()], choices=[('Push', 'Push'), ('Pull', 'Pull'), ('Legs', 'Legs'), ('Core', 'Core')])
     exercise_name = StringField('Exercise Name', validators=[DataRequired()])
     muscles = StringField('Muscles', validators=[DataRequired()])
-    exercise_difficulty = SelectField('Exercise Difficulty', validators=[DataRequired()], choices=[('easy', 'easy'), ('medium', 'medium'), ('difficult', 'difficult')])
+    exercise_difficulty = SelectField('Exercise Difficulty',
+                                      validators=[DataRequired()],
+                                      choices=[('easy', 'easy'),
+                                               ('medium', 'medium'),
+                                               ('difficult', 'difficult')])
     equipment = StringField('Equipment', validators=[DataRequired()])
-    exercise_instructions = TextAreaField('Exercise Instructions', validators=[DataRequired()])
+    exercise_instructions = TextAreaField('Exercise Instructions',
+                                          validators=[DataRequired()])
     submit = SubmitField('Add Exercise')
 
-"""
+
+
+category_name = SelectField('Exercise Group', validators=[DataRequired()],
+                                choices=[('Push', 'Push'),
+                                         ('Pull', 'Pull'),
+                                         ('Legs', 'Legs'),
+                                         ('Core', 'Core')])
 category_name = StringField('Exercise Group', validators=[DataRequired()])
-exercise_difficulty = StringField('Exercise Difficulty', validators=[DataRequired()])
+
+exercise_difficulty = StringField('Exercise Difficulty',
+                                      validators=[DataRequired()])
 """
