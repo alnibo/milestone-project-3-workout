2df217e5dccd47c19602e96fa5e09e9b98e73a1e
alnibo <alexander@bode-web.eu>
2020-03-06 00:53:51 +0000

    added dropdown, corrected javascript code for navbar and select field

--- static/js/script.js
+++ static/js/script.js
@@ -2,7 +2,22 @@
 
 $(document).ready(function () {
     $(".button-collapse").sideNav();
-    $('select').formSelect();
+    $('.dropdown-button').dropdown();
+    $('select').material_select();
+});
+
+// Search bar
+
+$(document).ready(function() {
+
+  $('.submit_on_enter').keydown(function(event) {
+    // enter has keyCode = 13, change it if you want to use another button
+    if (event.keyCode == 13) {
+      this.form.submit();
+      return false;
+    }
+  });
+
 });
 
 // Back to Top Button
@@ -29,3 +44,25 @@ if ($('#back-to-top').length) {
         }, 700);
     });
 }
+
+// Modal to confirm the deletion of an exercise
+
+window.onload = function() {
+    var modal = document.getElementById("delete-modal");
+    var btn = document.getElementById("delete-button");
+    var span = document.getElementsByClassName("delete-close")[0];
+
+    btn.onclick = function() {
+        modal.style.display = "block";
+    };
+
+    span.onclick = function() {
+        modal.style.display = "none";
+    };
+
+    window.onclick = function(event) {
+        if (event.target == modal) {
+            modal.style.display = "none";
+        }
+    };
+};
